3ba14a2cc49ad8a0735b572ec881817b17e12fff
LeeHanEum <LeeHanEum160924@outlook.kr>
2023-05-15 23:07:45 +0900

    pokemon commit

--- src/skillArr.java
+++ src/skillArr.java
@@ -1,6 +1,13 @@
 public class skillArr {
-    static skill[] skills{
-        new skill("");
-    }
+    static skill[] skills = {
+        new skill("", 0),
+        new skill("몸통박치기", 40),
+        new skill("덩굴채찍", 45),
+        new skill("잎날가르기", 55),
+        new skill("씨폭탄", 80),
+        new skill("솔라빔", 120),
+        new skill("기가드레인", 75),
+
+    };
 
 }
