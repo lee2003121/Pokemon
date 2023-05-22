23f842cdbf563bb8722646603b7b2a0bd3b2ef48
jungwoo9454 <jungwoo9454@gmail.com>
2023-05-22 18:40:29 +0900

    SceneState Add Mng class Add

---
+++ src/Framework/Scene.java
@@ -0,0 +1,23 @@
+package Framework;
+
+public class Scene {
+
+    public void Start()
+    {
+        System.out.println("Start");
+    }
+    public void Update()
+    {
+        //System.out.println("Update");
+    }
+
+    public void Draw()
+    {
+        //System.out.println("Draw");
+    }
+
+    public void Exit()
+    {
+        System.out.println("Exit");
+    }
+}
---
+++ src/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+import Mng.GameMng;
+
+public class Main {
+    public static void main(String[] args) {
+
+        GameMng.getInstance().GameLoop();
+    }
+}
