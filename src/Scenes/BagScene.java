441a9e21a52153844250d44f8a5fa91cee7eab82
ksh30918 <ksh30918@kakao.com>
2023-05-26 16:42:24 +0900

    edit Update func

--- src/Scenes/BagScene.java
+++ src/Scenes/BagScene.java
@@ -8,16 +8,20 @@ public class BagScene extends Scene {
     public  void Update()
     {
         super.Update();
-        switch (Integer.parseInt(inputData))
-        {
-
+        if(Integer.parseInt(inputData) == 0){//뒤로가기
+        }
+        else if(Integer.parseInt(inputData) >= Bag.getInstance().getNowItemCount()){
+            // 잘못된 입력
+        }
+        else {
+            // 아이템 사용처리
         }
     }
 
     @Override
     public void Draw() {
         super.Draw();
-        Bag bag = Bag.getInstance();
-        bag.printBag();
+        System.out.println("0. 뒤로가기");
+        Bag.getInstance().printBag();
     }
 }
