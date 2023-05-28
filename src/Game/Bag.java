9c22114b519a1380bbf10d348e96f0a343a05591
이종완 <60810332+lee2003121@users.noreply.github.com>
2023-05-28 18:04:25 +0900

    아이템, 코인 저장

--- src/Game/Bag.java
+++ src/Game/Bag.java
@@ -2,15 +2,17 @@ package Game;
 
 import Item.Item;
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class Bag {
     public final int MaxItemCount = 30;
-    private ArrayList<Item> ItemList;
+    private ArrayList<Item> itemList;
     private int coin;
+    private static final String FILE_NAME = "bag_data.ser";
 
     private Bag() {
-        ItemList = new ArrayList<Item>();
+        itemList = new ArrayList<>();
         coin = 0;
     }
 
@@ -22,32 +24,32 @@ public class Bag {
         return LazyHolder.instance;
     }
 
-    public int addItem(Item i) {
-        if (ItemList.size() >= MaxItemCount)
+    public int addItem(Item item) {
+        if (itemList.size() >= MaxItemCount)
             return -1;
         else {
-            ItemList.add(i);
+            itemList.add(item);
             return 0;
         }
     }
 
     public int getNowItemCount() {
-        return ItemList.size();
+        return itemList.size();
     }
 
     public void printBag() {
-        if (ItemList.size() == 0) {
+        if (itemList.size() == 0) {
             System.out.println("가방이 비어있습니다.");
         } else {
-            for (int i = 0; i < ItemList.size(); i++) {
-                System.out.println((i + 1) + ". " + ItemList.get(i).getName() + " " + ItemList.get(i).getDescription());
+            for (int i = 0; i < itemList.size(); i++) {
+                System.out.println((i + 1) + ". " + itemList.get(i).getName() + " " + itemList.get(i).getDescription());
             }
         }
     }
 
     public int useItem(int index) {
-        if (index > 0 && index <= ItemList.size()) {
-            ItemList.remove(index - 1);
+        if (index > 0 && index <= itemList.size()) {
+            itemList.remove(index - 1);
             return 0;
         }
         return -1;
@@ -67,5 +69,25 @@ public class Bag {
 
     public void deductCoin(int amount) {
         coin -= amount;
+    }
+
+    public void saveData() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
+            oos.writeObject(itemList);
+            oos.writeInt(coin);
+            System.out.println("데이터 저장 완료");
+        } catch (IOException e) {
+            System.out.println("데이터 저장 실패: " + e.getMessage());
+        }
+    }
+
+    public void loadData() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
+            itemList = (ArrayList<Item>) ois.readObject();
+            coin = ois.readInt();
+            System.out.println("데이터 로드 완료");
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("데이터 로드 실패: " + e.getMessage());
+        }
     }
 }
