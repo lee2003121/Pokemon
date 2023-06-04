9fa4bb7b9bf53ab0c6c7869dafdef4bb9b32dbb5
LeeHanEum <LeeHanEum160924@outlook.kr>
2023-06-04 21:22:48 +0900

    진화레벨업

---
+++ src/Game/Revolution.java
@@ -0,0 +1,28 @@
+package Game;
+import java.util.List;
+
+public class Revolution {
+    public boolean valLevel(PokemonInfo pokemon){
+        return pokemon.level>=10;
+    }
+
+    public boolean isPosRev(PokemonInfo prevPok, PokemonInfo nextPok){
+        return prevPok.hp > nextPok.hp;
+    }
+    public PokemonInfo addPokemon(PokemonInfo pokemon) {
+        List<PokemonInfo> allPokemon = Pokemon.getInfo();
+        PokemonInfo newPokemon = null;
+        if (isPosRev(allPokemon.get(allPokemon.indexOf(pokemon)), allPokemon.get((allPokemon.indexOf(pokemon)) + 1))) {
+            newPokemon = allPokemon.get((allPokemon.indexOf(pokemon)) + 1);
+        }
+        return newPokemon;
+    }
+
+    public List<PokemonInfo> checkRevolution(List<PokemonInfo> myPokemonList, PokemonInfo pokemon){
+        if(valLevel(pokemon)) {
+            myPokemonList.remove(pokemon);
+            myPokemonList.add(addPokemon(pokemon));
+        }
+        return myPokemonList;
+    }
+}
