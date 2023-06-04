97c7a881fd1bdbab904152d355b9e4b2ced88e7b
이종완 <60810332+lee2003121@users.noreply.github.com>
2023-06-05 03:43:59 +0900

    edit Delay in RecoveryScene

--- src/Scenes/RecoveryScene.java
+++ src/Scenes/RecoveryScene.java
@@ -6,7 +6,6 @@ import Framework.Scene;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
-
 public class RecoveryScene extends Scene {
     private List<PokemonInfo> allPokemon;
     private int dotCount;
@@ -20,19 +19,25 @@ public class RecoveryScene extends Scene {
     }
 
     public void recoverAllPokemon() {
-        new Thread(() -> {
-            while (!recoveryCompleted.get()) {
-                dotCount = (dotCount % 3) + 1;
+        // 별도의 Runnable 객체를 정의하여 스레드를 생성
+        Thread recoveryThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!recoveryCompleted.get()) {
+                    dotCount = (dotCount % 3) + 1;
 
-                String dots = repeatString(".", dotCount);
-                System.out.println("체력 회복 중" + dots);
-                try {
-                    Thread.sleep(510); // 0.3초간 대기
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    String dots = repeatString(".", dotCount);
+                    System.out.println("체력 회복 중" + dots);
+                    try {
+                        Thread.sleep(510); // 0.3초간 대기
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
-        }).start();
+        });
+
+        recoveryThread.start();
 
         try {
             Thread.sleep(3000); // 2초간 대기
