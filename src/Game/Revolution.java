6284c9314ba3f327cd42b3580a94ccab4c77601e
LeeHanEum <LeeHanEum160924@outlook.kr>
2023-06-04 21:40:14 +0900

    진화레벨업

--- src/Game/Revolution.java
+++ src/Game/Revolution.java
@@ -1,27 +1,24 @@
 package Game;
 import java.util.List;
 
+
 public class Revolution {
+
     public boolean valLevel(PokemonInfo pokemon){
         return pokemon.level>=10;
     }
-
     public boolean isPosRev(PokemonInfo prevPok, PokemonInfo nextPok){
-        return prevPok.hp > nextPok.hp;
+        return prevPok.hp < nextPok.hp;
     }
-    public PokemonInfo addPokemon(PokemonInfo pokemon) {
-        List<PokemonInfo> allPokemon = Pokemon.getInfo();
-        PokemonInfo newPokemon = null;
-        if (isPosRev(allPokemon.get(allPokemon.indexOf(pokemon)), allPokemon.get((allPokemon.indexOf(pokemon)) + 1))) {
-            newPokemon = allPokemon.get((allPokemon.indexOf(pokemon)) + 1);
-        }
-        return newPokemon;
-    }
-
     public List<PokemonInfo> checkRevolution(List<PokemonInfo> myPokemonList, PokemonInfo pokemon){
-        if(valLevel(pokemon)) {
-            myPokemonList.add(addPokemon(pokemon));
-            myPokemonList.remove(pokemon);
+        if(valLevel(pokemon)){
+            List<PokemonInfo> allPokemon = Pokemon.getInfo();
+            if(isPosRev(allPokemon.get(allPokemon.indexOf(pokemon)), allPokemon.get((allPokemon.indexOf(pokemon)) + 1))){
+                PokemonInfo newPokemon = allPokemon.get((allPokemon.indexOf(pokemon)) + 1);
+                myPokemonList.remove(pokemon);
+                myPokemonList.add(newPokemon);
+                System.out.println(pokemon + "이 " + newPokemon + "으로 진화 되었다!");
+            }
         }
         return myPokemonList;
     }
